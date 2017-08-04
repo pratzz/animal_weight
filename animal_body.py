38b42079f36d1e272751c41e5364bb523a01f791
Pratz <pratsnagare08@gmail.com>
2017-08-04 21:06:55 +0530

    predict animal's weight given animal's brain weight

--- animal_body.py
+++ animal_body.py
@@ -1,1 +1,17 @@
-#pratima
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+dataframe = pd.read_fwf("brain_body.txt")
+brain_values = dataframe[['Brain']]
+body_values = dataframe[['Body']]
+
+
+reg = linear_model.LinearRegression()
+reg.fit (brain_values,body_values)
+
+#visualize results
+plt.scatter(brain_values, body_values)
+plt.plot(brain_values, reg.predict(brain_values))
+plt.show()
